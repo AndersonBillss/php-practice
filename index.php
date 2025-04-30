6c9c6dd215e9fc41db519a6e26daa3aa8083edbf
AndersonBillss <anderson@thebillsfamily.com>
2025-04-29 19:10:15 -0600

    Added internal PHP code into index.php

--- index.php
+++ index.php
@@ -26,6 +26,57 @@
             </select>
 
             <button type="submit">Submit</button>
+
+            <?php 
+
+            $author = "Mahatma Gandhi";
+            
+            // switch ($author){
+            //     case "George Washington":
+            //         echo "You have only one way to convince others, listen to them.";
+            //         break;
+            //     case "Abraham Lincoln":
+            //         echo "Great men are ordinary men with extra ordinary determination.";
+            //         break;
+            //     case "Mahatma Gandhi":
+            //         echo "To double your successes, you might have to double your failure rate.";
+            //         break;
+            //     default:
+            //         echo "No quotes for this author!";
+            // }
+            // echo " - " . $author;
+            // echo "<br/>";
+            
+            // This does the same thing as the switch statement
+            $result = "No quotes for this author: " . $author;
+            $result = match ($author) {
+                "George Washington" => "You have only one way to convince others, listen to them.",
+                "Abraham Lincoln" => "Great men are ordinary men with extra ordinary determination.",
+                "Mahatma Gandhi" => "To double your successes, you might have to double your failure rate."
+            };
+            echo $result . " - " . $author;
+            echo "<br/>";
+
+
+            $bool = true;
+            $a = 1;
+            $b = 4;
+
+            if ($a < $b && $bool) {
+                echo $a . " is less than " . $b;
+            } else if ($a > $b && $bool){
+                echo $b . " is less than " . $a;
+            } else {
+                echo "Boolean is " . ($bool?"true":"false");
+            }
+            echo "<br/>";
+
+            $a = "Hello";
+            $b = "World!";
+            echo $a . " " . $b;
+
+            ?>
+
         </form>
     </main>
 </body>
