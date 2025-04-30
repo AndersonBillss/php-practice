184c99d07fe7f3de64cb163f7911e5a1be2dc9a9
AndersonBillss <anderson@thebillsfamily.com>
2025-04-29 18:30:02 -0600

    Created a form

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-$title = 'PHP is awesome!';
-require 'index.view.php';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>PHP Variables</title>
+</head>
+
+<body>
+    <main>
+        <form action="includes/formhandler.php" method="post">
+            <label for="firstname">FirstName?</label>
+            <input id="firstname" type="text" name="firstname" placeholder="Firstname...">
+
+            <label for="lastname">Lastname?</label>
+            <input id="lastname" type="text" name="lastname" placeholder="Lastname...">
+
+            <label for="favoritepet">Favorite Pet?</label>
+            <select id="favoritepet" name="favoritepet">
+                <option value="none">None</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="bird">Bird</option>
+            </select>
+
+            <button type="submit">Submit</button>
+        </form>
+    </main>
+</body>
+</html>
